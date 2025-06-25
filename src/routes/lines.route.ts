674bb842e19bca58fe7840b8d2511ae4814f9bf1
corentindautreme <co@Cos.localdomain>
2025-06-25 15:26:46 +0200

    Fix route description endpoint

--- src/routes/lines.route.ts
+++ src/routes/lines.route.ts
@@ -14,7 +14,7 @@ class LinesRoute {
 
     private initRoutes() {
         this.router.get('/', this.linesController.getAllLines.bind(this.linesController));
-        this.router.get('/describe', this.linesController.describeLineRoute.bind(this.linesController));
+        this.router.get('/describe-route', this.linesController.describeLineRoute.bind(this.linesController));
         this.router.get('/describe-line', this.linesController.describeLine.bind(this.linesController));
         this.router.get('/describe-all', this.linesController.describeAllLines.bind(this.linesController));
     }
